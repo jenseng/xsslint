56451d6aae780c805b532be3b1cf382eb05a8bc2
Jon Jensen <jenseng@gmail.com>
2014-12-18 23:12:27 -0700

    more directive tests

--- test/linterTest.js
+++ test/linterTest.js
@@ -1,6 +1,6 @@
 var assert = require("chai").assert;
 var Linter = require("../linter");
-
+var Config = require("../config");
 
 describe("Linter", function() {
   describe("comment directives", function() {
@@ -9,6 +9,26 @@ describe("Linter", function() {
       assert.deepEqual(
         linter.config.properties["safeString.identifier"],
         [{identifier: "foo"}, {identifier: /bar/}]
+      );
+    });
+
+    it("should augment existing settings", function() {
+      var config = new Config;
+      config.set("safeString.identifier", "foo");
+      var linter = new Linter("//xsslint safeString.identifier bar", config);
+      assert.deepEqual(
+        linter.config.properties["safeString.identifier"],
+        [{identifier: "foo"}, {identifier: "bar"}]
+      );
+    });
+
+    it("should replace existing settings if set with a !", function() {
+      var config = new Config;
+      config.set("!safeString.identifier", "foo");
+      var linter = new Linter("//xsslint safeString.identifier bar", config);
+      assert.deepEqual(
+        linter.config.properties["safeString.identifier"],
+        [{identifier: "bar"}]
       );
     });
   });
